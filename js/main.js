60b3ea423c269eec58f8e6369357737a54f0c49a
Maximo Belen <maximo.belen@audi.de>
2022-03-31 08:47:14 +0200

    add bitcoin task

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ function clickButton() {
   alert("When clicking this button we should show the description");
   return true;
 }
+
+function showBitcoinPrice() {
+  alert("When clicking this button we should show the Bitcoin price in EUR");
+  // use https://www.coingecko.com/en/api/documentation
+  return true;
+}
